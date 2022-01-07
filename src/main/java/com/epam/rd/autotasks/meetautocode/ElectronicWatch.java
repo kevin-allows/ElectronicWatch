1974f81a9308a4c5da1557cb310173d17d097a4d
Ilya Mankouski <ilya.mankouski@gmail.com>
2022-01-07 16:45:09 +0100

    Electronic Watch try_1

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,6 +6,10 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+        int minutes = (int)Math.floor((float)seconds/60);
+        int hours = (int)Math.floor((float)minutes/60);
+        seconds = seconds%60;
+        System.out.print(hours + ":" + minutes + ":" + seconds);
 
 
     }
